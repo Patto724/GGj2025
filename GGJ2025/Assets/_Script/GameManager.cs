11c00035a3e41cdb678840e88fc6a40403d8826c
Maydic489 <74528891+Maydic489@users.noreply.github.com>
2025-01-25 19:52:55 +0700

    Add basic random bubble spot that happen faster and faster

--- GGJ2025/Assets/_Script/GameManager.cs
+++ GGJ2025/Assets/_Script/GameManager.cs
@@ -1,11 +1,19 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject mainPlayer;
     public GameObject crowbarGO_PC;
     public GameObject crowbarGO_Mobile;
+
+    [SerializeField] float timeToSpawnBubble = 20f;
+
     public List<PickableScripableObject> pickableScripableObjects = new List<PickableScripableObject>();
+    [Space(10)]
+    [SerializeField] ParticleSystem bubbleParticle;
+    public List<Transform> bubbleSpots = new List<Transform>();
 
     public static GameManager instance;
     private void Awake()
@@ -18,6 +26,11 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(BubbleSpawnCycle());
     }
 
     public void AddPickableObject(PickableScripableObject pickableScripableObject)
@@ -39,5 +52,39 @@ public class GameManager : MonoBehaviour
                 return true;
             }
         } return false;
+    }
+
+    IEnumerator BubbleSpawnCycle()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeToSpawnBubble);
+            int spotIndex = FindClosestBubbleSpot(mainPlayer.transform.position);
+            bubbleParticle.transform.position = bubbleSpots[spotIndex].position;
+            bubbleParticle.Play();
+            yield return new WaitForSeconds(7f);
+            bubbleParticle.Stop();
+            timeToSpawnBubble -= timeToSpawnBubble * 0.1f;
+            if(timeToSpawnBubble < 5f)
+            {
+                timeToSpawnBubble = 5f;
+            }
+        }
+    }
+
+    int FindClosestBubbleSpot(Vector3 playerPos)
+    {
+        float minDistance = Mathf.Infinity;
+        int index = 0;
+        for (int i = 0; i < bubbleSpots.Count; i++)
+        {
+            float distance = Vector3.Distance(playerPos, bubbleSpots[i].position);
+            if (distance < minDistance && distance > 2.5f)
+            {
+                minDistance = distance;
+                index = i;
+            }
+        }
+        return index;
     }
 }
